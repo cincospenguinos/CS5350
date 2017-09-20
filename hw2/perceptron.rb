419ae2be0fd75a0e413840a64c14e228fca58b5a
Andre LaFleur <andre.lafleur@utah.edu>
2017-09-20 08:35:07 -0600

    Setup 5-fold cross validation to find ideal hyper parameters

--- hw2/perceptron.rb
+++ hw2/perceptron.rb
@@ -6,55 +6,75 @@ class Perceptron
 
   attr_reader :learning_rate
   attr_reader :weights
+  attr_reader :mistakes
+  attr_reader :tests
 
   def initialize(learning_rate)
     @learning_rate = learning_rate.to_f
-    @weights = [1.0]
+    @weights = [ 1.0 ] # The first element is the bias weight
+    @mistakes = 0
+    @tests = 0
   end
 
   ## Perceptron algorithms
 
   def simple_perceptron(examples)
     perceptron(examples) do |example|
-      [ @weights.size, example.size ].min.times do |i|
-        @weights[i] += @learning_rate * example.label * example.features[i]
-      end
+      [ @weights.size, example.size ].min.times { |i| @weights[i] += @learning_rate * example.label * example.features[i] }
     end
   end
 
-  def dynamic_learning_perceptron
-    perceptron do
-      raise RuntimeError, 'Implement this!'
+  def dynamic_learning_perceptron(examples)
+    perceptron do |example|
+      # TODO: This
     end
   end
 
-  def margin_perceptron
-    perceptron do
-      raise RuntimeError, 'Implement this!'
-    end
+  def margin_perceptron(examples)
+    # perceptron do
+    #   raise RuntimeError, 'Implement this!'
+    # end
   end
 
-  def averaged_perceptron
-    perceptron do
-      raise RuntimeError, 'Implement this!'
-    end
+  def averaged_perceptron(examples)
+    # perceptron do
+    #   raise RuntimeError, 'Implement this!'
+    # end
+  end
+
+  # TODO: The fifth perceptron, if there's time
+
+  ## Tests the current weight set against the example provided. Adds one to mistakes
+  def test_for(example)
+    @mistakes += 1 if is_mistake(example)
+    @tests += 1
+  end
+
+  def self.all_flavors
+    Perceptron.instance_methods(false) - [ :learning_rate, :weights, :mistakes, :tests, :test_for ]
+  end
+
+  def self.single_hyper_param_flavors
+    all_flavors - [ :margin_perceptron ]
   end
 
   ## Other methods
 
   private
 
+  ## Resets the perceptron instance so we get a new setup for each run
+  def reset
+    @weights.each { |i| @weights[i] = 0.0 }
+    @weights[0] = 1.0
+    @mistakes = 0
+    @tests = 0
+  end
+
   # The perceptron algorithm that runs the show
   def perceptron(training_examples)
+    reset # To ensure that this instance doesn't have weights that stomp on each other
     training_examples.each do |example|
-      # Initialization step
-      while @weights.size < example.size do
-        @weights << 0.0 
-      end
-
-      if is_mistake(example)
-        yield example
-      end
+      yield example if is_mistake(example)
     end
 
     @weights
@@ -68,6 +88,9 @@ class Perceptron
   end
 
   def is_mistake(example)
+    while @weights.size < example.size do
+      @weights << 0.0 
+    end
     example.label * weight_product(example) <= 0.0
   end
 end
--- hw2/solution.rb
+++ hw2/solution.rb
@@ -3,37 +3,93 @@
 require_relative 'example'
 require_relative 'perceptron'
 
-# NOTE: This is a proof of concept showing how to grab all of the examples in a dataset
-examples = []
+raise RuntimeError, 'Only 4 potential flavors should be available!' if Perceptron.all_flavors.size != 4 # Quick sanity check
 
-File.foreach('Dataset/phishing.dev') do |line|
-  label = line.split(/\s+/)[0]
-  feature_set = line.split(/\s+/) - [ label ]
-  
-  e = Example.new(label)
+# TODO: Look at assignment info about setting the bias, and do what it tells you in the Perceptron class
 
-  i = 1
-  feature_set.each do |value_pair|
-    j = value_pair.split(':')[0].to_f
+## Gets examples from file matching filename passed
+def get_examples_from(filename)
+  examples = []
+
+  File.foreach(filename) do |line|
+    label = line.split(/\s+/)[0]
+    feature_set = line.split(/\s+/) - [ label ]
     
-    while i < j do 
-      e << 0 
+    e = Example.new(label)
+
+    i = 1
+    feature_set.each do |value_pair|
+      j = value_pair.split(':')[0].to_f
+      
+      while i < j do 
+        e << 0 
+        i += 1
+      end
+
+      e << value_pair.split(':')[1].to_f
       i += 1
     end
 
-    e << value_pair.split(':')[1].to_f
-    i += 1
+    examples << e
   end
 
-  examples << e
+  examples
 end
 
-perceptron = Perceptron.new(1.0)
-perceptron.simple_perceptron(examples)
-puts perceptron.weights.inspect
+## Tests against the file passed using the perceptron provided
+def test_against(perceptron, file_name)
+  # TODO: This
+end
+
+## Returns the ideal hyper parameter given the perceptron flavor and the set of hyper_parameters
+def ideal_hyper_parameter(hyper_parameters, flavor, epochs=10)
+  puts "Ideal Hyper Param for #{flavor}\n"
+
+  shuffle_param = Random.new(25)
+  mistakes = {}
+
+  # Five-fold cross validation
+  5.times do |test_index|
+    puts "#{test_index}..."
+    training_examples = []
+    i = (test_index + 1) % 5
+
+    while i < 5 do
+      training_examples += get_examples_from("Dataset/CVSplits/training0#{i}.data") if i != test_index
+      i += 1
+    end
+
+    test_examples = get_examples_from("Dataset/CVSplits/training0#{test_index}.data")
+
+    epochs.times do
+      hyper_parameters.each do |hyper_param|
+        perceptron = Perceptron.new(hyper_param)
+        perceptron.send(flavor, training_examples.shuffle(random:shuffle_param))
+        test_examples.each { |e| perceptron.test_for(e) }
+
+        mistakes[perceptron.mistakes] = hyper_param
+      end
+    end
+
+    
+  end
+
+  mistakes[mistakes.keys.min]
+end
 
 # TODO:  Run cross validation for ten epochs for each hyper-parameter combination to get the 
 # best hyper-parameter setting.
+hyper_params = {}
+
+Perceptron.all_flavors.each do |flavor|
+  if Perceptron.single_hyper_param_flavors.include?(flavor)
+    hyper_params[flavor] = ideal_hyper_parameter([1, 0.1, 0.01], flavor)
+  else
+    # TODO: This
+  end
+end
+
+puts hyper_params.inspect
 
 # TODO: Train the classifier for 20 epochs. At the end of each training epoch, you should measure
 # the accuracy of the classifier on the development set. For the averaged Perceptron,
